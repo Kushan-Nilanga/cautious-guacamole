dc07c023480ca1578a59e7fae49bc6c6550ee61f
Kushan <dknathalage@gmail.com>
2021-07-28 15:58:07 +1000

    changes

--- script.sh
+++ script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
-yum update -y
-yum install curl
-yum install git
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
-sudo apt-get install -y nodejs
+sudo yum update -y
+sudo yum install -y curl
+sudo yum install -y git
+sudo yum install -y gcc-c++ make
+
+
+curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash - 
+sudo yum install -y nodejs
 
 git clone https://github.com/Kushan-Nilanga/cautious-guacamole-load-balance.git
 cd cautious-guacamole-load-balance
