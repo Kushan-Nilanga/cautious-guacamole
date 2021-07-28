c87b8dd260e4547c71e26bafbe31c924adbdb5c0
Kushan <dknathalage@gmail.com>
2021-07-28 18:47:34 +1000

    changes

--- loadbalance/client.js
+++ loadbalance/client.js
@@ -2,7 +2,7 @@ const http = require('http');
 setInterval(loadtest, 50); //time is in ms
 
 function loadtest(){
-    http.get('http://localhost:3000', (res) => { 
+    http.get('http://sit-314-lb-1503578701.ap-southeast-2.elb.amazonaws.com', (res) => { 
         res.on('data', function (chunk) {
             console.log('' + chunk); 
         });
